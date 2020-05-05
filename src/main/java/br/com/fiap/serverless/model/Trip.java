e873ba6add3abb77d391d806fdfdbf18824a28e7
Richel Sensineli <richelsensineli@Richels-MacBook-Pro.local>
2020-05-05 18:50:32 -0300

    Implementação inicial do modelo de dados

---
+++ src/main/java/br/com/fiap/serverless/model/Trip.java
@@ -0,0 +1,13 @@
+package br.com.fiap.serverless.model;
+
+import java.time.LocalDate;
+
+public class Trip {
+
+    private Long id;
+    private LocalDate date;
+    private String country;
+    private String city;
+    private String repositoryURL;
+
+}
---
+++ src/main/java/br/com/fiap/serverless/service/TripService.java
@@ -0,0 +1,4 @@
+package br.com.fiap.serverless.service;
+
+public class TripService {
+}
