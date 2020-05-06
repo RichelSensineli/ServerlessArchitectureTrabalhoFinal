df35a33e6898ebe5734c0030106202491ced001e
Richel Sensineli <richelsensineli@Richels-MacBook-Pro.local>
2020-05-06 15:20:36 -0300

    Alteração de QueryExpression para ScanExpression para consulta de Trips no período, adicionada autogeneratedKey e URL

--- src/main/java/br/com/fiap/serverless/dao/TripRepository.java
+++ src/main/java/br/com/fiap/serverless/dao/TripRepository.java
@@ -3,23 +3,32 @@ package br.com.fiap.serverless.dao;
 import br.com.fiap.serverless.model.Trip;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 public class TripRepository {
 
     private static final DynamoDBMapper mapper = DynamoDBManager.mapper();
 
     public Trip save(final Trip trip) {
+    	
+    	Random generator = new Random();
+    	int randomNumber = generator.nextInt(899999) + 100000;
+ 
+    	String url = "www.aws."+trip.getCountry()+"-"+trip.getCity()+"-"+trip.getDate()+randomNumber;
+    	trip.setUrl(url.replace(" ", ""));
+
         mapper.save(trip);
         return trip;
     }
 
     public List<Trip> findByPeriod(final String starts, final String ends) {
-
+    	
         final Map<String, AttributeValue> eav = new HashMap<String, AttributeValue>();
         eav.put(":val1", new AttributeValue().withS(starts));
         eav.put(":val2", new AttributeValue().withS(ends));
@@ -27,14 +36,14 @@ public class TripRepository {
         final Map<String, String> expression = new HashMap<>();
         expression.put("#date", "date");
 
-        final DynamoDBQueryExpression<Trip> queryExpression = new DynamoDBQueryExpression<Trip>()
-                .withKeyConditionExpression("#date between :val1 and :val2")
+        final DynamoDBScanExpression scanExpression = new DynamoDBScanExpression()
+                .withFilterExpression("#date between :val1 and :val2")
                 .withExpressionAttributeValues(eav)
                 .withExpressionAttributeNames(expression);
 
-        final List<Trip> studies = mapper.query(Trip.class, queryExpression);
+        final List<Trip> trips = mapper.scan(Trip.class, scanExpression);
 
-        return studies;
+        return trips;
     }
 
     public List<Trip> findById(String id) {
